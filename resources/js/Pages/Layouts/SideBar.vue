201a588d67e6ea647796279e0e372f337ef925ee
TripleKay <triplekay.k3@gmail.com>
2022-08-01 14:59:27 +0630

    active state

--- resources/js/Pages/Layouts/SideBar.vue
+++ resources/js/Pages/Layouts/SideBar.vue
@@ -7,7 +7,7 @@
             </div>
             <div class="card-body">
                 <div class="" v-for="tag in $page.props.tags" :key="tag.id">
-                    <Link :href="'/?tag='+tag.slug" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;">{{ tag.name }}</Link>
+                    <Link :href="'/?tag='+tag.slug" :class="{ 'active': $page.url === '/?tag='+tag.slug }" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;">{{ tag.name }}</Link>
                 </div>
             </div>
         </div>
@@ -18,11 +18,11 @@
             </div>
             <div class="card-body">
                 <div class="">
-                    <Link :href="route('profile.edit')" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;"><i class="fas fa-user-edit me-2"></i>Edit Profile</Link>
+                    <Link :href="route('profile.edit')" :class="{ 'active': route().current('profile.edit') }" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;"><i class="fas fa-user-edit me-2"></i>Edit Profile</Link>
                     <Link href="" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;"><i class="fas fa-lock me-2"></i>Change Password</Link>
-                    <Link :href="route('profile.userQuestion')" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;">
+                    <Link :href="route('profile.userQuestion')" :class="{ 'active': route().current('profile.userQuestion') }" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;">
                         <i class="fas fa-question-circle me-2"></i>Your Questions</Link>
-                    <Link :href="route('showSaveQuestion')" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;"><i class="fas fa-heart me-2"></i>Saved Questions</Link>
+                    <Link :href="route('showSaveQuestion')" :class="{ 'active': route().current('showSaveQuestion') }" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;"><i class="fas fa-heart me-2"></i>Saved Questions</Link>
                 </div>
             </div>
         </div>
@@ -31,8 +31,8 @@
         <div class="card border-0 mb-3" style="border-radius: 15px;">
             <div class="card-body">
                 <div class="">
-                    <Link href="/?type=answered" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;">Answered Questions</Link>
-                    <Link href="/?type=unanswered" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;">UnAnswered Questions</Link>
+                    <Link href="/?type=answered" :class="{ 'active': $page.url === '/?type=answered'}" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;">Answered Questions</Link>
+                    <Link href="/?type=unanswered" :class="{ 'active': $page.url === '/?type=unanswered' }" class="btn w-100 text-start mb-2" style="border: .5px solid #00000010; border-radius: 15px;">UnAnswered Questions</Link>
                 </div>
             </div>
         </div>
@@ -60,6 +60,10 @@ import { Link } from '@inertiajs/inertia-vue3'
     }
 </script>
 
-<style lang="stylus" scoped>
+<style scoped>
+.active {
+    background: var(--bs-primary);
+    color: #fff;
+}
 
 </style>
